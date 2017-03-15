665caa4bb5f519c68d8aedc92475de6b99c56a7b
Nathan Perry <nateperry333@gmail.com>
2017-03-15 14:13:07 -0400

    adds index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const path = require('path');
+
+exports.netsuiteWsdlPath = path.join(__dirname, 'src');
+exports.netsuiteWsdl = path.join(exports.netsuiteWsdlPath, 'netsuite.wsdl');
