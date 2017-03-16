ce1ae53572d1164ff46f04e0f0f99c7d4c68a9d5
Nathan Perry <nateperry333@gmail.com>
2017-03-16 09:15:57 -0400

    Added version and renamed some exports

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 const path = require('path');
 
-exports.netsuiteWsdlPath = path.join(__dirname, 'src');
-exports.netsuiteWsdl = path.join(exports.netsuiteWsdlPath, 'netsuite.wsdl');
+exports.path = path.join(__dirname, 'src');
+exports.wsdl = exports.netsuiteWsdl = path.join(exports.path, 'netsuite.wsdl');
+exports.version = '2016_2';
