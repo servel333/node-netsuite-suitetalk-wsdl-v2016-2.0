76591db7a04c60db43793cac3d695e5b32d129ca
Nathan Perry <nateperry333@gmail.com>
2017-03-16 15:34:46 -0400

    Renamed wsdl and netsuiteWsdl to just file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 exports.path = path.join(__dirname, 'src');
-exports.wsdl = exports.netsuiteWsdl = path.join(exports.path, 'netsuite.wsdl');
+exports.file = path.join(exports.path, 'netsuite.wsdl');
 exports.version = '2016_2';
 
 exports.operations = [
