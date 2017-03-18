460d92350ef897f449667c7f50a5a71efd6468cd
Nathan Perry <nateperry333@gmail.com>
2017-03-18 12:28:12 -0400

    .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [ "error", 2 ],
+        "linebreak-style": [ "error", "unix" ],
+        "semi": [ "error", "always" ]
+    }
+};
