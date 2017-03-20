471024560dc15a232818a8820a19b1e2464899fd
Nathan Perry <nateperry333@gmail.com>
2017-03-20 09:17:18 -0400

    Subfolder for WSDL-v2016-2.0

--- netsuite-suitetalk-wsdl-v2016-2.0/index.js
+++ netsuite-suitetalk-wsdl-v2016-2.0/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const WsdlSchema = require('./WsdlSchema');
 
 exports.path = path.join(__dirname, 'src');
 exports.file = path.join(exports.path, 'netsuite.wsdl');
@@ -10,3 +11,14 @@ exports.operations = require('./operations.json');
 
 // Namespace prefix : Namespace URI
 exports.namespaces = require('./namespaces.json');
+
+var _schema;
+exports.getSchema = function(cb){
+  if (_schema) { cb(null, _schema); }
+
+  WsdlSchema.getSchema(this.file, function(err, schema){
+    if(err) { return cb(err); }
+    _schema = schema;
+    cb(null, _schema);
+  });
+};
