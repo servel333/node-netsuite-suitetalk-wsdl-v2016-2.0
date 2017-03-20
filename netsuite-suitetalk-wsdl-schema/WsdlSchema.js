1c142dfce7c6e48501e4478fa70f6f0052a10f92
Nathan Perry <nateperry333@gmail.com>
2017-03-20 09:28:42 -0400

    Added lodash use to WsdlSchema file

--- netsuite-suitetalk-wsdl-schema/WsdlSchema.js
+++ netsuite-suitetalk-wsdl-schema/WsdlSchema.js
@@ -1,33 +1,11 @@
 'use strict';
 
+const _ = require('lodash');
 const soap = require('soap');
-
-var eachArr = function(arr, cb){
-  for (var i = 0, len = arr.length; i < len; i++) {
-    cb(arr[i], i, arr);
-  }
-};
-
-var eachObj = function(obj, cb){
-  for (var key in obj) {
-    if (!obj.hasOwnProperty(key)) continue;
-    cb(obj[key], key, obj);
-  }
-};
-
-var get = function(obj, keypath) {
-  var paths = keypath.split('.');
-
-  eachArr(paths, function(path){
-    obj = obj && obj[path];
-  });
-
-  return obj;
-};
 
 var WsdlSchema = module.exports = exports = function(client) {
   this.client = client;
-  var schemas = get(this.client, 'wsdl.definitions.schemas');
+  var schemas = _.get(this.client, 'wsdl.definitions.schemas');
   this._types = this.loadTypes(schemas);
 };
 
@@ -35,9 +13,9 @@ WsdlSchema.prototype.loadTypes = function(schemas){
 
   var types = {};
 
-  eachObj(schemas, function(schema, namespaceUri){
-    var complexTypes = get(schema, 'complexTypes');
-    eachObj(complexTypes, function(typeDef, typeName){
+  _.each(schemas, function(schema, namespaceUri){
+    var complexTypes = _.get(schema, 'complexTypes');
+    _.each(complexTypes, function(typeDef, typeName){
       types[typeName] = {
         name: typeName,
         namespaceUri: namespaceUri,
